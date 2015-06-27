ad08dd00f086d535556199fb1e0e0649b5661dfc
HarriKnox <harri.alpha96@gmail.com>
2015-06-27 16:17:08 -0700

    Final changes for new website redesign

--- formatPage.js
+++ formatPage.js
@@ -14,19 +14,7 @@ var paddingAmount = 20;
 
 $('document').ready(function()
 {
-	var pageName = window.location.pathname.substring(1);
-	var pageTitle = "Home";
-	if (pageName !== "" && pageName !== "index.html")
-	{
-		for (var i = 0; i < navbarButtons.length; i++)
-		{
-			if (navbarButtons[i].href === pageName)
-			{
-				pageTitle = navbarButtons[i].name;
-				break;
-			}
-		}
-	}
+	var pageTitle = $('#pageName').html();
 	
 	$('head').append('<title>Coder by Nature: ' + pageTitle + '</title><link type="text/css" rel="stylesheet" href="stylesheet.css" />');
 	
@@ -39,9 +27,16 @@ $('document').ready(function()
 	
 	$('body').prepend('<div id="header-container"><div id="title"><h1 style="margin-bottom:0px;">Hello friends and family</h1><h3 style="margin-top:0px;">My name is Harrison and I\'m a programmer, not by trade but by nature</h3></div><div id="navbar"></div></div>');
 	$('#header-container').width(pageWidth + (2 * paddingAmount) + 10);
+	
 	$navbar = $('#navbar');
 	for (var i = 0; i < navbarButtons.length; i++)
 	{
-		$navbar.append('<div class="navbar-button">' + navbarButtons[i].name + '</div>');
+		var selected = (navbarButtons[i].name === pageTitle);
+		
+		var inside = '<div ' + (selected ? 'id="navbar-button-selected"' : '') + 'class="navbar-button">' + navbarButtons[i].name + '</div>';
+		if (!selected) inside = '<a href="' + navbarButtons[i].href + '">' + inside + '</a>';
+		
+		$navbar.append(inside);
 	}
 });
+
