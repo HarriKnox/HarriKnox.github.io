607080f5dba7d655379a5ccc91d2bc1d6a60f071
HarriKnox <harri.alpha96@gmail.com>
2015-07-04 04:52:08 -0700

    Added splashes and a new Languages page, as well as edited content on the other pages

--- formatPage.js
+++ formatPage.js
@@ -5,36 +5,58 @@ var NavbarButton = function(name, href)
 };
 
 var navbarButtons = [
-	new NavbarButton("Home", "index.html"),
-	new NavbarButton("Projects", "projects.html"),
+	new NavbarButton('Home', 'index.html'),
+	new NavbarButton('Projects', 'projects.html'),
+	new NavbarButton('My Languages', 'languages.html'),
 ];
 
-var pageWidth = 1000;
-var paddingAmount = 20;
+var splashes = [
+	"Now includes<br/>complementary splashes",
+	"Work in progress",
+	"Check out the navbar",
+	"Made by Harrison Knox",
+	"Call me Harri",
+	"Powered by jQuery",
+	"Powered by electrons",
+	"Now available in English",
+];
+
+var getSplash = function()
+{
+	return splashes[Math.floor(Math.random() * splashes.length)];
+};
 
 $('document').ready(function()
 {
-	var pageTitle = $('#pageName').html();
+	var $content = $('#content-container');
+	var pageTitle = $content.attr('page');
 	
-	$('head').append('<title>Coder by Nature: ' + pageTitle + '</title><link type="text/css" rel="stylesheet" href="stylesheet.css" />');
+	$('head').append('<title>Coder by Nature: ' + pageTitle + '</title>')
+			.append('<link type="text/css" rel="stylesheet" href="stylesheet.css" />');
+	$content.prepend('<h1 style="margin:0px;">' + pageTitle + '</h1>');
 	
-	$content = $('#content-container');
-	$content.css({
-		width : pageWidth,
-		padding : paddingAmount,
-	});
-	$content.prepend('<h2 style="margin:0px;">' + pageTitle + '</h2>');
-	
-	$('body').prepend('<div id="header-container"><div id="title"><h1 style="margin-bottom:0px;">Hello friends and family</h1><h3 style="margin-top:0px;">My name is Harrison and I\'m a programmer, not by trade but by nature</h3></div><div id="navbar"></div></div>');
-	$('#header-container').width(pageWidth + (2 * paddingAmount) + 10);
+	var $body = $('body')
+	$body.prepend('<div id="navbar"></div>');
+	$body.prepend(
+		'<table id="header-container">' +
+			'<td id="title">' +
+				'<h1 style="margin-bottom:0px;">Hello friends and family</h1>' +
+				'<h3 style="margin-top:0px;">My name is Harrison and I\'m a programmer, not by trade but by nature</h3>' +
+			'</td>' +
+			'<td id="splash">' +
+				getSplash() + 
+			'</td>' +
+		'</table>'
+	);
 	
 	$navbar = $('#navbar');
 	for (var i = 0; i < navbarButtons.length; i++)
 	{
-		var selected = (navbarButtons[i].name === pageTitle);
+		var current = navbarButtons[i];
+		var selected = (current.name === pageTitle);
 		
-		var inside = '<div ' + (selected ? 'id="navbar-button-selected"' : '') + 'class="navbar-button">' + navbarButtons[i].name + '</div>';
-		if (!selected) inside = '<a href="' + navbarButtons[i].href + '">' + inside + '</a>';
+		var inside = '<div ' + (selected ? 'id="navbar-button-selected"' : '') + 'class="navbar-button">' + current.name + '</div>';
+		if (!selected) inside = '<a href="' + current.href + '">' + inside + '</a>';
 		
 		$navbar.append(inside);
 	}
