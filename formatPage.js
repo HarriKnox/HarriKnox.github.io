ff05464036669f7c811aa057459ea4df8b0ad431
HarriKnox <harri.alpha96@gmail.com>
2015-07-08 12:47:10 -0700

    Added date-specific splashes, removed the automatic title in the content-container, made menus slide instead of just toggle, and tried to use tabindex

--- formatPage.js
+++ formatPage.js
@@ -1,23 +1,40 @@
-var splashes = [
-	'Now includes<br/>complementary splashes',
-	'Work in progress',
-	'Check out the navbar',
-	'Made by Harrison Knox',
-	'Made from scratch',
-	'Made in California',
-	'Call me Harri',
-	'Powered by jQuery',
-	'Powered by electrons',
-	'Now available in English',
-	'<span style="color:#FF7F50;">Coral</span> <span style="color:#DC143C;">Crimson</span> <span style="color:#FF8C00;">DarkOrange</span><br />' +
-	'<span style="color:#FFA500;">Orange</span> <span style="color:#FF4500;">OrangeRed</span> <span style="color:#FF0000;">Red</span>',
-	'<a href="https://github.com/HarriKnox">Fork me on GitHub</a>',
-	'Requires internet access',
-	'Ad-free',
-];
-
-var getSplash = function()
+var getDay = function(month, date) // Pass in month in [1..12] (adjusted for ease of use) and date in [1..31]
 {
+	if (typeof month !== 'number' || typeof date !== 'number') // If either parameter isn't a number, it sets the month and date to today
+	{
+		var now = new Date();
+		month = now.getMonth() + 1;
+		date = now.getDate();
+	}
+	return month * 100 + date;
+};
+
+var getSplash = function(pageTitle)
+{
+	var today = getDay();
+	if (today === 111) return 'Happy Birthday Harri!';
+	if (today === 1008) return 'Happy Birthday Courtney!';
+	if (today === 126) return '<div style="transform:rotate(180deg);">Happy Australia Day!</div>';
+	if (today >= 1200) return 'Happy &lt;insert holiday here&gt;!';
+	if (today === 1031) return '<span style="color:#ffa500;">BOO!</span>';
+	var splashes = [
+		'Now includes<br/>complementary splashes',
+		'Work in progress',
+		'Check out the navbar',
+		'Made by Harrison Knox',
+		'Made from scratch',
+		'Made in California',
+		'Call me Harri',
+		'Powered by jQuery',
+		'Powered by electrons',
+		'Now available in English',
+		'<span style="color:#FF7F50;">Coral</span> <span style="color:#DC143C;">Crimson</span> <span style="color:#FF8C00;">DarkOrange</span><br />' +
+		'<span style="color:#FFA500;">Orange</span> <span style="color:#FF4500;">OrangeRed</span> <span style="color:#FF0000;">Red</span>',
+		'<a href="https://github.com/HarriKnox">Fork me on GitHub</a>',
+		'Requires internet access',
+		'Ad-free',
+		'Open source',
+	];
 	return splashes[Math.floor(Math.random() * splashes.length)];
 };
 
@@ -41,6 +58,7 @@ var navbar = [
 	new Menu('Programming',
 	[
 		new Button('Languages', 'languages.html'),
+		new Button('Lua', 'lua.html'),
 	]),
 ];
 
@@ -56,7 +74,10 @@ var positionMenu = function(menuName)
 
 var hideAllMenus = function()
 {
-	$('.navbar-menu').hide();
+	$('.navbar-menu').each(function()
+	{
+		$(this).slideUp(100);//hide();
+	});
 };
 
 var getHome = function()
@@ -71,12 +92,10 @@ var UP_ARROW = '&#9650;'
 $('document').ready(function()
 {
 	/// Get page title ///
-	var $content = $('#content-container');
-	var pageTitle = $content.attr('page');
+	var pageTitle = $('#content-container').attr('page');
 	
 	/// Write page title in <head> and as a header ///
-	$('head').append('<title>Coder by Nature: ' + pageTitle + '</title>');
-	$content.prepend('<h1 style="margin:0px;">' + pageTitle + '</h1>');
+	$('head').append('<link type="text/css" rel="stylesheet" href="stylesheet.css" /><title>Coder by Nature: ' + pageTitle + '</title>');
 	
 	/// Build header ///
 	var $body = $('body')
@@ -84,13 +103,13 @@ $('document').ready(function()
 	var title = '<h1 style="margin-bottom:0px;">Hello friends and family</h1>' +
 		'<h3 style="margin-top:0px;">My name is Harrison and I\'m a programmer, not by trade but by nature</h3>';
 	if (pageTitle !== 'Home')
-		title = '<a href="' + getHome() + '" id="link-home">' + title + '</a>';
+		title = '<a href="' + getHome() + '" id="link-home" title="Home">' + title + '</a>';
 	$body.prepend(
 		'<table id="header-container">' +
 			'<td id="title" class="squish">' + title +
 			'</td><td></td>' +
 			'<td id="splash" class="squish">' +
-				getSplash() + 
+				getSplash(pageTitle) + 
 			'</td>' +
 		'</table>'
 	);
@@ -102,7 +121,7 @@ $('document').ready(function()
 		var menuName = navbar[m].name;
 		var menu = navbar[m].menu;
 		
-		$navbar.append('<div id="' + menuName + '" class="navbar-menu-button">' + menuName + '</div>');
+		$navbar.append('<div id="' + menuName + '" class="navbar-menu-button" ' + 'tabindex="' + (m + 1) + '">' + menuName + '</div>');
 		$navbar.append('<div id="' + menuName + '-menu" class="navbar-menu"></div>');
 		
 		var $menu = $('#' + menuName + '-menu');
@@ -130,7 +149,9 @@ $('document').ready(function()
 		var hidden = $menu.is(':hidden');
 		positionMenu(menuName);
 		hideAllMenus();
-		$menu.toggle(hidden);
+		if (hidden) $menu.slideDown(100);
+		else $menu.slideUp(100);
+		//$menu.toggle(hidden);
 	});
 	
 	/// Make all external links open new tabs ///
