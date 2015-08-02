d7d9519a89fff4c53a3cad878f2b7cc11752979d
HarriKnox <harri.alpha96@gmail.com>
2015-08-01 17:45:09 -0700

    Grouped tasks to load scripts and sheets

--- scripts/loadScripts.js
+++ scripts/loadScripts.js
@@ -1,8 +1,25 @@
 var file = location.href.match(/^file/) !== null;
 
 document.write(
-	'<script src="' + (file ? 'scripts/' : 'http://ajax.googleapis.com/ajax/libs/jquery/1.11.3/') + 'jquery.min.js"></script>' +
-	'<script src="scripts/formatPage.js"></script>' +
-	'<link type="text/css" rel="stylesheet" href="stylesheets/stylesheet.css" />' +
-	'<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no" />'
+	'<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">'
 );
+
+var scripts = [
+	(file ? 'scripts/' : 'http://ajax.googleapis.com/ajax/libs/jquery/1.11.3/') + 'jquery.min.js',
+	'scripts/formatPage.js',
+];
+scripts.forEach(function(script)
+{
+	document.write('<script src="' + script + '<"></script>');
+});
+
+
+var stylesheets = [
+	'header.css',
+	'content.css',
+	'stylesheet.css',
+];
+stylesheets.forEach(function(sheet)
+{
+	document.write('<link type="text/css" rel="stylesheet" href="stylesheets/' + sheet + '">');
+});
