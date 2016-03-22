810ad226425e2b6b88953eb75c1e4249bccada7d
HarriKnox <harri.alpha96@gmail.com>
2016-03-21 23:31:09 -0700

    Added margin to number outputs

--- scripts/numberNameRectangles/illionGroupName.js
+++ scripts/numberNameRectangles/illionGroupName.js
@@ -5,7 +5,7 @@
 		var $groupNumberElement = $('div.illion-group-name-div');
 		$groupNumberElement.html('<input class="illion-group-number" onkeydown="if (event.keyCode === 13) $(\'.illion-group-number-button\').click();"/>' +
 			'<button class="illion-group-number-button" type="button" onclick="illion_group_number_button_onclick()">Get Name</button>' +
-			'<div class="illion-group-name-result"><em>The group name will appear here</em></div>');
+			'<div class="illion-group-name-result" style="margin:0em 2em;"><em>The group name will appear here</em></div>');
 		
 		var notLoaded = typeof io === 'undefined' || typeof io.harriknox === 'undefined' || typeof io.harriknox.NumberName === 'undefined';
 		if (notLoaded)
