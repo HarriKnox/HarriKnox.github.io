25613e95cdcdb78fa5059f52c84c649ee13cacf6
HarriKnox <harri.alpha96@gmail.com>
2016-03-30 17:22:44 -0700

    Updated with most recent commit hash for Number Namer

--- scripts/NumberNamer/loadNumberNamer.js
+++ scripts/NumberNamer/loadNumberNamer.js
@@ -1,1 +1,1 @@
-document.write('<script src="https://cdn.rawgit.com/HarriKnox/Number-Name/3cd5c264f2443b77b93af2785788bc6229924919/out/main.js"></script>');
+document.write('<script src="https://cdn.rawgit.com/HarriKnox/Number-Name/1d185bbcc321139c667db82b70c82ee0f96d297f/out/main.js"></script>');
