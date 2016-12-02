5e59e1fe9d64c9c687f8d1bb1b7afa60ee873ee4
Harrison Patrick Knox <harri.alpha96@gmail.com>
2016-12-01 19:34:09 -0800

    Change tabs to spaces (fight me)

--- scripts/NumberNamer/illionGroupNameRectangle.js
+++ scripts/NumberNamer/illionGroupNameRectangle.js
@@ -1,54 +1,54 @@
 (function()
 {
-	if (typeof illion_group_number_button_onclick === 'undefined')
-	{
-		illion_group_number_button_onclick = function()
-		{
-			var value = document.getElementsByClassName('illion-group-name-input')[0].value;
-			var result = '<em>The group name will appear here</em>';
-			if (value !== '')
-			{
-				try
-				{
-					result = '<span style="word-wrap:break-word;">' + io.harriknox.NumberNamer.illion_group_name(value) + '</span>';
-				}
-				catch (e)
-				{
-					result = '<span style="color:red;font-weight:bold;">Error: ';
-					if (value.match(/[^\d\.\-]/)) result += 'Value contains non-numerals';
-					else if (!value.match(/^-?\d+(\.\d+)?$/)) result += 'Value not in number format';
-					else if (value.match(/^-/) || value.match(/^0+$/)) result += 'Value is not positive';
-					else if (value.match(/\./)) result += 'Value is not an integer';
-					else result += e.message;
-					result += '</span>';
-				}
-			}
-			document.getElementsByClassName('illion-group-name-result')[0].innerHTML = result;
-		};
-		
-		var notLoaded = typeof io === 'undefined' || typeof io.harriknox === 'undefined' || typeof io.harriknox.NumberNamer === 'undefined';
-		
-		var ignInput = document.createElement('input');
-		ignInput.className = 'illion-group-name-input';
-		ignInput.pattern = '^0*[1-9]\\d*$';
-		ignInput.value = 0;
-		ignInput.setAttribute('onkeydown', 'if (event.keyCode === 13) illion_group_number_button_onclick();');
-		
-		var ignButton = document.createElement('input');
-		ignButton.type = 'button';
-		ignButton.className = 'illion-group-name-button';
-		ignButton.value = 'Submit';
-		ignButton.setAttribute('onclick', 'illion_group_number_button_onclick()');
-		
-		var ignResult = document.createElement('div');
-		ignResult.className = 'illion-group-name-result';
-		ignResult.style = 'margin:0em 2em;';
-		ignResult.innerHTML = (notLoaded ? '<span style="color:red;font-weight:bold;">Error: Number-Namer could not be loaded</span>' : '<em>The group name will appear here</em>');
-		
-		var parent = (function(coll) { return coll[coll.length - 1]; })(document.getElementsByTagName('script')).parentNode;
-		
-		parent.appendChild(ignInput);
-		parent.appendChild(ignButton);
-		parent.appendChild(ignResult);
-	}
+    if (typeof illion_group_number_button_onclick === 'undefined')
+    {
+        illion_group_number_button_onclick = function()
+        {
+            var value = document.getElementsByClassName('illion-group-name-input')[0].value;
+            var result = '<em>The group name will appear here</em>';
+            if (value !== '')
+            {
+                try
+                {
+                    result = '<span style="word-wrap:break-word;">' + io.harriknox.NumberNamer.illion_group_name(value) + '</span>';
+                }
+                catch (e)
+                {
+                    result = '<span style="color:red;font-weight:bold;">Error: ';
+                    if (value.match(/[^\d\.\-]/)) result += 'Value contains non-numerals';
+                    else if (!value.match(/^-?\d+(\.\d+)?$/)) result += 'Value not in number format';
+                    else if (value.match(/^-/) || value.match(/^0+$/)) result += 'Value is not positive';
+                    else if (value.match(/\./)) result += 'Value is not an integer';
+                    else result += e.message;
+                    result += '</span>';
+                }
+            }
+            document.getElementsByClassName('illion-group-name-result')[0].innerHTML = result;
+        };
+        
+        var notLoaded = typeof io === 'undefined' || typeof io.harriknox === 'undefined' || typeof io.harriknox.NumberNamer === 'undefined';
+        
+        var ignInput = document.createElement('input');
+        ignInput.className = 'illion-group-name-input';
+        ignInput.pattern = '^0*[1-9]\\d*$';
+        ignInput.value = 0;
+        ignInput.setAttribute('onkeydown', 'if (event.keyCode === 13) illion_group_number_button_onclick();');
+        
+        var ignButton = document.createElement('input');
+        ignButton.type = 'button';
+        ignButton.className = 'illion-group-name-button';
+        ignButton.value = 'Submit';
+        ignButton.setAttribute('onclick', 'illion_group_number_button_onclick()');
+        
+        var ignResult = document.createElement('div');
+        ignResult.className = 'illion-group-name-result';
+        ignResult.style = 'margin:0em 2em;';
+        ignResult.innerHTML = (notLoaded ? '<span style="color:red;font-weight:bold;">Error: Number-Namer could not be loaded</span>' : '<em>The group name will appear here</em>');
+        
+        var parent = (function(coll) { return coll[coll.length - 1]; })(document.getElementsByTagName('script')).parentNode;
+        
+        parent.appendChild(ignInput);
+        parent.appendChild(ignButton);
+        parent.appendChild(ignResult);
+    }
 })();
--- scripts/NumberNamer/powerOfTenToStringRectangle.js
+++ scripts/NumberNamer/powerOfTenToStringRectangle.js
@@ -1,54 +1,54 @@
 (function()
 {
-	if (typeof power_of_ten_button_onclick === 'undefined')
-	{
-		power_of_ten_button_onclick = function()
-		{
-			var value = document.getElementsByClassName('power-of-ten-input')[0].value;
-			var result = '<em>The number\'s name will appear here</em>';
-			if (value !== '')
-			{
-				try
-				{
-					result = '<span style="word-wrap:break-word;">' + io.harriknox.NumberNamer.power_of_10_to_string(value) + '</span>';
-				}
-				catch (e)
-				{
-					result = '<span style="color:red;font-weight:bold;">Error: ';
-					if (value.match(/[^\d\.\-]/)) result += 'Value contains non-numerals';
-					else if (!value.match(/^-?\d+(\.\d+)?$/)) result += 'Value not in number format';
-					else if (value.match(/^-/)) result += 'Value is negative';
-					else if (value.match(/\./)) result += 'Value is not an integer';
-					else result += e.message;
-					result += '</span>';
-				}
-			}
-			document.getElementsByClassName('power-of-ten-result')[0].innerHTML = result;
-		};
-		
-		var notLoaded = typeof io === 'undefined' || typeof io.harriknox === 'undefined' || typeof io.harriknox.NumberNamer === 'undefined';
-		
-		var potInput = document.createElement('input');
-		potInput.className = 'power-of-ten-input';
-		potInput.pattern = '^\\d+$';
-		potInput.value = 0;
-		potInput.setAttribute('onkeydown', 'if (event.keyCode === 13) power_of_ten_button_onclick();');
-		
-		var potButton = document.createElement('input');
-		potButton.type = 'button';
-		potButton.className = 'power-of-ten-button';
-		potButton.value = 'Submit';
-		potButton.setAttribute('onclick', 'power_of_ten_button_onclick()');
-		
-		var potResult = document.createElement('div');
-		potResult.className = 'power-of-ten-result';
-		potResult.style = 'margin:0em 2em;';
-		potResult.innerHTML = (notLoaded ? '<span style="color:red;font-weight:bold;">Error: Number-Namer could not be loaded</span>' : '<em>The number\'s name will appear here</em>');
-		
-		var parent = (function(coll) { return coll[coll.length - 1]; })(document.getElementsByTagName('script')).parentNode;
-		
-		parent.appendChild(potInput);
-		parent.appendChild(potButton);
-		parent.appendChild(potResult);
-	}
+    if (typeof power_of_ten_button_onclick === 'undefined')
+    {
+        power_of_ten_button_onclick = function()
+        {
+            var value = document.getElementsByClassName('power-of-ten-input')[0].value;
+            var result = '<em>The number\'s name will appear here</em>';
+            if (value !== '')
+            {
+                try
+                {
+                    result = '<span style="word-wrap:break-word;">' + io.harriknox.NumberNamer.power_of_10_to_string(value) + '</span>';
+                }
+                catch (e)
+                {
+                    result = '<span style="color:red;font-weight:bold;">Error: ';
+                    if (value.match(/[^\d\.\-]/)) result += 'Value contains non-numerals';
+                    else if (!value.match(/^-?\d+(\.\d+)?$/)) result += 'Value not in number format';
+                    else if (value.match(/^-/)) result += 'Value is negative';
+                    else if (value.match(/\./)) result += 'Value is not an integer';
+                    else result += e.message;
+                    result += '</span>';
+                }
+            }
+            document.getElementsByClassName('power-of-ten-result')[0].innerHTML = result;
+        };
+        
+        var notLoaded = typeof io === 'undefined' || typeof io.harriknox === 'undefined' || typeof io.harriknox.NumberNamer === 'undefined';
+        
+        var potInput = document.createElement('input');
+        potInput.className = 'power-of-ten-input';
+        potInput.pattern = '^\\d+$';
+        potInput.value = 0;
+        potInput.setAttribute('onkeydown', 'if (event.keyCode === 13) power_of_ten_button_onclick();');
+        
+        var potButton = document.createElement('input');
+        potButton.type = 'button';
+        potButton.className = 'power-of-ten-button';
+        potButton.value = 'Submit';
+        potButton.setAttribute('onclick', 'power_of_ten_button_onclick()');
+        
+        var potResult = document.createElement('div');
+        potResult.className = 'power-of-ten-result';
+        potResult.style = 'margin:0em 2em;';
+        potResult.innerHTML = (notLoaded ? '<span style="color:red;font-weight:bold;">Error: Number-Namer could not be loaded</span>' : '<em>The number\'s name will appear here</em>');
+        
+        var parent = (function(coll) { return coll[coll.length - 1]; })(document.getElementsByTagName('script')).parentNode;
+        
+        parent.appendChild(potInput);
+        parent.appendChild(potButton);
+        parent.appendChild(potResult);
+    }
 })();
