7cae3737027dc3e6878bde33df60d4b3ccec4a2c
Harrison Patrick Knox <harri.alpha96@gmail.com>
2017-05-09 01:02:52 -0700

    Remove JS dependence for dynamic <pre> width

--- scripts/format/content.js
+++ scripts/format/content.js
@@ -9,15 +9,6 @@
    var $document = $(document);
    var $window = $(window);
    
-   /** Sets the max-width for <pre> blocks so they aren't wider than the content-container **/
-   var setPreMaxWidth = function()
-   {
-      var $pre = $('pre');
-      var padding = parseInt($pre.css('padding-left')) + parseInt($pre.css('padding-right'));
-      var width = $('.content-container').width();
-      $pre.css('max-width', width - padding);
-   };
-   
    $document.ready(function()
    {
       var $content = $('.content-container');
@@ -26,15 +17,8 @@
       var GROUPED = '<span class="grouped">$1</span>';
       var innards = $content.html();
       innards = innards.replace(                      /((?:[^\w\s>]|&.+?;)+<code>(?:[^<]|<(?!\/code>))*?<\/code>)/g,                          GROUPED)
-                   .replace(                                         /(<code>(?:[^<]|<(?!\/code>))*?<\/code>(?:[^<\w\s]|&.+?;)+)/g,       GROUPED)
-                   .replace(/<span class="grouped">((?:[^\w\s>]|&.+?;)+<code>(?:[^<]|<(?!\/code>))*?<\/code>)<\/span>([^<\w\s]|&.+?;)+/g, GROUPED.replace(/\$1/, '$$1$$2'));
+                       .replace(                                         /(<code>(?:[^<]|<(?!\/code>))*?<\/code>(?:[^<\w\s]|&.+?;)+)/g,       GROUPED)
+                       .replace(/<span class="grouped">((?:[^\w\s>]|&.+?;)+<code>(?:[^<]|<(?!\/code>))*?<\/code>)<\/span>([^<\w\s]|&.+?;)+/g, GROUPED.replace(/\$1/, '$$1$$2'));
       $content.html(innards);
-      
-      
-      /** Make all pre blocks scroll horizontally when the content is too wide for the window **/
-      setPreMaxWidth();
-      
-      /** Adjust sizes of pre blocks when the window is resized **/
-      $window.resize(setPreMaxWidth);
    });
 })(jQuery);
